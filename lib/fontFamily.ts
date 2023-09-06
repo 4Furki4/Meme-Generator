0be47b9c4b6c7820a40d171220444d05a1014137
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-06 18:20:12 +0300

    add: all font families required
    the function that returns font family class is complete as well

--- lib/fontFamily.ts
+++ lib/fontFamily.ts
@@ -5,18 +5,15 @@ export const getFontFamilies = () => {
         "Inter",
         "Impact",
         "Arial",
-        "Comic Sans MS",
+        "Open Sans",
         "Helvetica",
         "Times New Roman",
-        "Times",
-        "Courier New",
-        "Courier",
+        "Courier Prime",
         "Verdana",
         "Georgia",
         "Palatino",
         "Garamond",
         "Bookman",
-        "Trebuchet MS",
         "Arial Black"
     ]
 }
@@ -29,42 +26,126 @@ export const timesNewRoman = localFont({
 export const arial = localFont({
     src: "../fonts/arial.woff2",
 })
+
 export const courierPrime = localFont({
     src: [
         {
-            path: "../fonts/CourierPrime-Regular.ttf",
+            path: "../fonts/CourierPrime/CourierPrime-Regular.ttf",
             weight: "400",
             style: "normal"
         },
         {
-            path: "../fonts/CourierPrime-Bold.ttf",
+            path: "../fonts/CourierPrime/CourierPrime-Bold.ttf",
             weight: "700",
             style: "bold"
         },
         {
-            path: "../fonts/CourierPrime-Italic.ttf",
+            path: "../fonts/CourierPrime/CourierPrime-Italic.ttf",
             weight: "400",
             style: "italic"
         },
         {
-            path: "../fonts/CourierPrime-BoldItalic.ttf",
+            path: "../fonts/CourierPrime/CourierPrime-BoldItalic.ttf",
+            weight: "700",
+        }
+    ]
+})
+
+export const impact = localFont({
+    src: "../fonts/impact.woff2"
+})
+export const openSans = localFont({
+    src: [
+        {
+            path: "../fonts/OpenSans/OpenSans-Regular.woff2",
+            weight: "400",
+            style: "normal"
+        },
+        {
+            path: "../fonts/OpenSans/OpenSans-Bold.woff2",
+            weight: "700",
+            style: "bold"
+        },
+        {
+            path: "../fonts/OpenSans/OpenSans-Italic.woff2",
+            weight: "400",
+            style: "italic"
+        },
+        {
+            path: "../fonts/OpenSans/OpenSans-BoldItalic.woff2",
             weight: "700",
             style: "italic"
         }
     ]
 })
 
+export const helvetica = localFont({
+    src: [
+        {
+            path: "../fonts/Helvetica/Helvetica-Regular.woff2",
+            weight: "400",
+            style: "normal"
+        },
+        {
+            path: "../fonts/Helvetica/Helvetica-Bold.woff2",
+            weight: "700",
+            style: "bold"
+        },
+        {
+            path: "../fonts/Helvetica/Helvetica-Italic.woff2",
+            weight: "400",
+            style: "italic"
+        },
+        {
+            path: "../fonts/Helvetica/Helvetica-BoldItalic.woff2",
+            weight: "700",
+            style: "italic"
+        }
+    ]
+})
 
+export const georgia = localFont({
+    src: "../fonts/georgia.woff2",
+})
 
+export const palatino = localFont({
+    src: "../fonts/palatino.woff2",
+})
 
+export const garamond = localFont({
+    src: "../fonts/garamond.woff2",
+})
+
+export const bookman = localFont({
+    src: "../fonts/bookman.woff2",
+})
+export const arialBlack = localFont({
+    src: "../fonts/arial-black.woff2",
+})
 export function getFontFamilyClass(fontFamily: string) {
     switch (fontFamily) {
         case "times new roman":
             return timesNewRoman.className
         case "inter":
             return inter.className
-        case "courier":
+        case "courier prime":
             return courierPrime.className
+        case "impact":
+            return impact.className
+        case "open sans":
+            return openSans.className
+        case "helvetica":
+            return helvetica.className
+        case "georgia":
+            return georgia.className
+        case "palatino":
+            return palatino.className
+        case "garamond":
+            return garamond.className
+        case "bookman":
+            return bookman.className
+        case "arial black":
+            return arialBlack.className
         default:
             return arial.className
     }
